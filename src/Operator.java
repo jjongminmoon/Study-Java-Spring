a2244c9e9abccbf352b683247ca30333a5533964
jjongminmoon <mjm7042@kakao.com>
2023-11-04 22:06:39 +0900

    2023/11/04 VDA 실습

--- src/Operator.java
+++ src/Operator.java
@@ -2,10 +2,12 @@ public class Operator {
     public static void main(String[] args) {
         // Q. 두 과목의 점수를 이용하여 총점과 평균을 출력
         int kor, eng;
-        kor=77;
-        eng=87;
+         kor=77;
+         eng=87;
+        int tot;
+        tot=kor+eng;
 
-        System.out.println("총점:"+(kor+eng));
-        System.out.println("총점:"+(kor+eng)/2);
+        System.out.println("총점:"+tot);
+        System.out.println("총점:"+tot/2);
     }
 }
