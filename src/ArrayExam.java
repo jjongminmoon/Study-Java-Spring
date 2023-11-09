34a9eaa2f51bf30ec5216cdbb4c5b138922ab9ff
jjongminmoon <mjm7042@kakao.com>
2023-11-09 22:22:00 +0900

    2023/11/09 2차원 배열

--- src/ArrayExam.java
+++ src/ArrayExam.java
@@ -1,10 +1,10 @@
 public class ArrayExam {
     public static void main(String[] args) {
+        // Q. char[] 배열에 APPLE이라는 문자를 apple로 출력하시오.
         char[] c={'A','P','P','L','E'};
 
         for(int i=0; i<c.length; i++) {
-            c[i]=(char)(c[i]+32);
-            System.out.println(c[i]);
+            System.out.print((char)(c[i]+32));
         }
     }
 }
